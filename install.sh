48bee46dc283e8026930051cc9b4442ec5270e9d
Gerome Matilla <gerome.matilla07@gmail.com>
2020-08-11 11:13:43 +0800

    install.sh: fix type

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ user_pref("layers.acceleration.force-enabled", true);
 user_pref("gfx.webrender.all", true);
 user_pref("gfx.webrender.enabled", true);
 user_pref("svg.context-properties.content.enabled", true);
-user_pred("layout.css.backdrop-filter.enabled", true);
+user_pref("layout.css.backdrop-filter.enabled", true);
 EOL
 		if [[ $? -eq 0 ]];
 		then
